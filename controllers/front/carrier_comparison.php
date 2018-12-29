2c1c3fbf893dfb34ff74d1e7b56f4ba672a53c79
MathiasReker <mathias@reker.dk>
2018-12-29 23:39:25 +0100

    No blank lines after phpdoc

--- controllers/front/carrier_comparison.php
+++ controllers/front/carrier_comparison.php
@@ -23,7 +23,6 @@
  * @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
  * International Registered Trademark & Property of PrestaShop SA
  */
-
 require_once(dirname(__FILE__) . '/../../../../config/config.inc.php');
 require_once(dirname(__FILE__) . '/../../../../init.php');
 require_once(dirname(__FILE__) . '/../../ps_carriercomparison.php');
